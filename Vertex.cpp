8f98aaa5378990e1d3437d4c6818148bebb95c7a
bryce-s <bryces@umich.edu>
2017-12-27 16:15:42 -0500

    class plan

---
+++ Vertex.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bryce Smith on 12/27/17.
+//
+
+#include "Vertex.h"
---
+++ Vertex.h
@@ -0,0 +1,14 @@
+//
+// Created by Bryce Smith on 12/27/17.
+//
+
+#ifndef GSV_TOOL_VERTEX_H
+#define GSV_TOOL_VERTEX_H
+
+
+class Vertex {
+
+};
+
+
+#endif //GSV_TOOL_VERTEX_H
